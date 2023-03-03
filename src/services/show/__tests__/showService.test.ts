c7149df126cd20ab9d91b39b4cc1c729553391e9
eduardoarad <araujocarlos893@gmail.com>
2023-03-03 15:04:39 -0300

    Aula 4

--- src/services/show/__tests__/showService.test.ts
+++ src/services/show/__tests__/showService.test.ts
@@ -1,27 +1,54 @@
-import {showMocks} from 'test/mocks/showMocks';
+import {api} from '../../api';
+import {episode1, episode2, episode22, episode23, episodeList} from '../mocks';
+// import {showMocks} from 'test/mocks/showMocks';
 import {showService} from '../showService';
 
 describe('showService', () => {
   describe('getEpisodes', () => {
-    test('when API return episode list return all season names', async () => {
-      const groupedEpisodes = await showService.getEpisodes('250');
-
-      expect(groupedEpisodes.seasonNames.includes('1')).toBeTruthy();
-      expect(groupedEpisodes.seasonNames.includes('2')).toBeTruthy();
-
-      expect(groupedEpisodes.seasonNames.length).toBe(2);
+    beforeAll(() => {
+      jest.spyOn(api, 'get').mockResolvedValue({data: episodeList});
     });
-    test('when API return episode list return the episodes grouped by season', async () => {
-      const groupedEpisodes = await showService.getEpisodes('250');
+    test('when API return episode list return all season nomes', async () => {
+      const groupEpisode = await showService.getEpisodes('250');
 
-      const temp1 = groupedEpisodes.seasons[1];
-      const temp2 = groupedEpisodes.seasons[2];
+      expect(groupEpisode.seasonNames.includes('1')).toBeTruthy();
+      expect(groupEpisode.seasonNames.includes('2')).toBeTruthy();
+      // expect(groupEpisode.seasonNames).toEqual(['1', '2']);
+      // expect(groupEpisode.seasonNames.length).toBe(2);
 
-      expect(temp1[0]).toEqual(showMocks.episode1);
-      expect(temp1[1]).toEqual(showMocks.episode2);
-
-      expect(temp2[0]).toEqual(showMocks.episode22);
-      expect(temp2[1]).toEqual(showMocks.episode23);
+      // expect(spyFn).toBeCalledTimes(1);
     });
+    test('when API return episode list return the episode grouped by season', async () => {
+      const groupEpisode = await showService.getEpisodes('250');
+
+      const season1 = groupEpisode.seasons[1];
+      const season2 = groupEpisode.seasons[2];
+
+      expect(season1.includes(episode1)).toBeTruthy();
+      expect(season1.includes(episode2)).toBeTruthy();
+
+      expect(season2.includes(episode22)).toBeTruthy();
+      expect(season2.includes(episode23)).toBeTruthy();
+    });
+    // test('when API return episode list return all season names', async () => {
+    //   const groupedEpisodes = await showService.getEpisodes('250');
+
+    //   expect(groupedEpisodes.seasonNames.includes('1')).toBeTruthy();
+    //   expect(groupedEpisodes.seasonNames.includes('2')).toBeTruthy();
+
+    //   expect(groupedEpisodes.seasonNames.length).toBe(2);
+    // });
+    // test('when API return episode list return the episodes grouped by season', async () => {
+    //   const groupedEpisodes = await showService.getEpisodes('250');
+
+    //   const temp1 = groupedEpisodes.seasons[1];
+    //   const temp2 = groupedEpisodes.seasons[2];
+
+    //   expect(temp1[0]).toEqual(showMocks.episode1);
+    //   expect(temp1[1]).toEqual(showMocks.episode2);
+
+    //   expect(temp2[0]).toEqual(showMocks.episode22);
+    //   expect(temp2[1]).toEqual(showMocks.episode23);
+    // });
   });
 });
---
+++ src/services/show/mocks.ts
@@ -0,0 +1,119 @@
+import {Episode} from '../../models/EpisodeModel';
+
+export const episode1: Episode = {
+  _links: {
+    self: {
+      href: 'https://www.tvmaze.com/episodes/20849/kirby-buckets-1x01-cars-buses-and-lawnmowers',
+    },
+  },
+  airdate: '2014-10-20',
+  airstamp: '2014-10-21T00:00:00+00:00',
+  airtime: '20:00',
+  id: 20849,
+  image: {
+    medium:
+      'https://static.tvmaze.com/uploads/images/medium_landscape/303/759608.jpg',
+    original:
+      'https://static.tvmaze.com/uploads/images/original_untouched/303/759608.jpg',
+  },
+  name: 'Cars, Buses and Lawnmowers',
+  number: 1,
+  rating: {
+    average: null,
+  },
+  runtime: 30,
+  season: 1,
+  summary:
+    '<p>Kirby learns about a character design contest featuring the ultimate prize - a chance to meet idol Mac McCallister and debut an animated character on TV. He plans to attend with Fish and Eli, but when they unexpectedly get stranded at school hours before the contest, they devise a plan that accidently places them in the middle of "Clown Town," an abandoned area of the city where creepy clowns reign. Meanwhile, Dawn realizes Kirby is using an embarrassing drawing of her titled "Dawnzilla" as his submission for the contest, so she and Belinda do everything in their power to stop him.</p>',
+  type: 'regular',
+  url: 'https://www.tvmaze.com/episodes/20849/kirby-buckets-1x01-cars-buses-and-lawnmowers',
+};
+
+export const episode2: Episode = {
+  _links: {
+    self: {
+      href: 'https://www.tvmaze.com/episodes/20849/kirby-buckets-1x01-cars-buses-and-lawnmowers',
+    },
+  },
+  airdate: '2014-10-20',
+  airstamp: '2014-10-21T00:00:00+00:00',
+  airtime: '20:00',
+  id: 20849,
+  image: {
+    medium:
+      'https://static.tvmaze.com/uploads/images/medium_landscape/303/759608.jpg',
+    original:
+      'https://static.tvmaze.com/uploads/images/original_untouched/303/759608.jpg',
+  },
+  name: 'Cars, Buses and Lawnmowers',
+  number: 2,
+  rating: {
+    average: null,
+  },
+  runtime: 30,
+  season: 1,
+  summary:
+    '<p>Kirby learns about a character design contest featuring the ultimate prize - a chance to meet idol Mac McCallister and debut an animated character on TV. He plans to attend with Fish and Eli, but when they unexpectedly get stranded at school hours before the contest, they devise a plan that accidently places them in the middle of "Clown Town," an abandoned area of the city where creepy clowns reign. Meanwhile, Dawn realizes Kirby is using an embarrassing drawing of her titled "Dawnzilla" as his submission for the contest, so she and Belinda do everything in their power to stop him.</p>',
+  type: 'regular',
+  url: 'https://www.tvmaze.com/episodes/20849/kirby-buckets-1x01-cars-buses-and-lawnmowers',
+};
+
+export const episode22: Episode = {
+  _links: {
+    self: {
+      href: 'https://www.tvmaze.com/episodes/20849/kirby-buckets-1x01-cars-buses-and-lawnmowers',
+    },
+  },
+  airdate: '2014-10-20',
+  airstamp: '2014-10-21T00:00:00+00:00',
+  airtime: '20:00',
+  id: 20849,
+  image: {
+    medium:
+      'https://static.tvmaze.com/uploads/images/medium_landscape/303/759608.jpg',
+    original:
+      'https://static.tvmaze.com/uploads/images/original_untouched/303/759608.jpg',
+  },
+  name: 'Cars, Buses and Lawnmowers',
+  number: 2,
+  rating: {
+    average: null,
+  },
+  runtime: 30,
+  season: 2,
+  summary:
+    '<p>Kirby learns about a character design contest featuring the ultimate prize - a chance to meet idol Mac McCallister and debut an animated character on TV. He plans to attend with Fish and Eli, but when they unexpectedly get stranded at school hours before the contest, they devise a plan that accidently places them in the middle of "Clown Town," an abandoned area of the city where creepy clowns reign. Meanwhile, Dawn realizes Kirby is using an embarrassing drawing of her titled "Dawnzilla" as his submission for the contest, so she and Belinda do everything in their power to stop him.</p>',
+  type: 'regular',
+  url: 'https://www.tvmaze.com/episodes/20849/kirby-buckets-1x01-cars-buses-and-lawnmowers',
+};
+
+export const episode23: Episode = {
+  _links: {
+    self: {
+      href: 'https://www.tvmaze.com/episodes/20849/kirby-buckets-1x01-cars-buses-and-lawnmowers',
+    },
+  },
+  airdate: '2014-10-20',
+  airstamp: '2014-10-21T00:00:00+00:00',
+  airtime: '20:00',
+  id: 20849,
+  image: {
+    medium:
+      'https://static.tvmaze.com/uploads/images/medium_landscape/303/759608.jpg',
+    original:
+      'https://static.tvmaze.com/uploads/images/original_untouched/303/759608.jpg',
+  },
+  name: 'Cars, Buses and Lawnmowers',
+  number: 3,
+  rating: {
+    average: null,
+  },
+  runtime: 30,
+  season: 2,
+  summary:
+    '<p>Kirby learns about a character design contest featuring the ultimate prize - a chance to meet idol Mac McCallister and debut an animated character on TV. He plans to attend with Fish and Eli, but when they unexpectedly get stranded at school hours before the contest, they devise a plan that accidently places them in the middle of "Clown Town," an abandoned area of the city where creepy clowns reign. Meanwhile, Dawn realizes Kirby is using an embarrassing drawing of her titled "Dawnzilla" as his submission for the contest, so she and Belinda do everything in their power to stop him.</p>',
+  type: 'regular',
+  url: 'https://www.tvmaze.com/episodes/20849/kirby-buckets-1x01-cars-buses-and-lawnmowers',
+};
+
+export const episodeList = [episode1, episode2, episode22, episode23];
